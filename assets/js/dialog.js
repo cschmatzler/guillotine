38fbbc7a886caf82c8ed23139dce131c6faa4208
Christoph Schmatzler <christoph@medium.place>
2024-05-28 21:23:38 +0200

    try something
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- assets/js/dialog.js
+++ assets/js/dialog.js
@@ -27,20 +27,18 @@ export const Dialog = {
   },
 
   render() {
-    const trigger = this.el.querySelector("[data-part='trigger']");
-    const backdrop = this.el.querySelector("[data-part='backdrop']");
-    const positioner = this.el.querySelector("[data-part='positioner']");
-    const content = this.el.querySelector("[data-part='content']");
-    const title = this.el.querySelector("[data-part='title']");
-    const description = this.el.querySelector("[data-part='description']");
-    const closeTrigger = this.el.querySelector("[data-part='close-trigger']");
-
-    if (trigger) spreadProps(trigger, this.api.triggerProps);
-    if (backdrop) spreadProps(backdrop, this.api.backdropProps);
-    if (positioner) spreadProps(positioner, this.api.positionerProps);
-    if (content) spreadProps(content, this.api.contentProps);
-    if (title) spreadProps(title, this.api.titleProps);
-    if (description) spreadProps(description, this.api.descriptionProps);
-    if (closeTrigger) spreadProps(closeTrigger, this.api.closeTriggerProps);
+    parts = ["trigger", "backdrop", "positioner", "content", "title", "description", "close-trigger"]
+    parts.forEach((part) => renderPart(part));
   },
+
+  renderPart(name) {
+    const part = this.el.querySelector(`[data-part='${name}']`);
+    if (part) spreadProps(part, this.api[`${kebabToCamel(part)}Props`]);
+  }
 };
+
+function kebabToCamel(str) {
+  return str.replace(/-([a-z])/g, function(_match, letter) {
+    return letter.toUpperCase();
+  });
+}
