3d2e5a582198ecb0674f29a717db783ab386b9e3
Christoph Schmatzler <christoph@medium.place>
2024-05-28 20:19:00 +0200

    release
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,7 @@
-import { Accordion } from "./accordion";
 import { Menu } from "./menu";
 
 const Hooks = {
-  Accordion: Accordion,
   Menu: Menu,
 };
 
-export { Accordion, Menu, Hooks };
+export { Menu, Hooks };
