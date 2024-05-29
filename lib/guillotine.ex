b11530bc4ccc7e289021d109f3f4267d24e77b38
Christoph Schmatzler <christoph@medium.place>
2024-05-29 22:43:16 +0200

    update dialog as well
    Signed-off-by: Christoph Schmatzler <christoph@medium.place>

--- lib/guillotine.ex
+++ lib/guillotine.ex
@@ -4,29 +4,10 @@ defmodule Guillotine do
              |> String.split("<!-- MDOC !-->")
              |> Enum.fetch!(1)
 
-  import Phoenix.Component
-
   @external_resource "README.md"
 
   @doc false
   def nanoid do
     Nanoid.generate(16, "0123456789abcdefghijklmnopqrstuvwxyz")
-  end
-
-  @doc false
-  def render_as_tag_or_component(assigns, extras \\ %{}) do
-    assigns =
-      assigns
-      |> Map.merge(Map.get(assigns, :rest, %{}))
-      |> Map.delete(:rest)
-      |> Map.merge(extras)
-
-    ~H"""
-    <%= if is_function(@as) do %>
-      <%= Phoenix.LiveView.TagEngine.component(@as, Map.delete(assigns, :as), {__ENV__.module, __ENV__.function, __ENV__.file, __ENV__.line}) %>
-    <% else %>
-      <.dynamic_tag name={@as} {Map.delete(assigns, :as)} />
-    <% end %>
-    """
   end
 end
--- lib/guillotine/dialog.ex
+++ lib/guillotine/dialog.ex
@@ -5,94 +5,63 @@ defmodule Guillotine.Dialog do
   ## Example
 
   ```heex
-  <.dialog>
-    <.dialog_trigger class="rounded-md bg-blue-500 px-3 py-1.5 text-sm text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">
-      Open dialog
-    </.dialog_trigger>
-    <.dialog_backdrop class="absolute inset-0 w-full h-full bg-gray-200"></.dialog_backdrop>
-    <.dialog_positioner class="fixed inset-0 z-10 w-screen overflow-y-auto flex min-h-full items-center justify-center p-4">
-      <.dialog_content class="w-full max-w-md rounded-xl bg-white p-6 outline-0">
-        <.dialog_title class="text-base font-medium">Dialog</.dialog_title>
-        <.dialog_description class="mt-2 text-sm">Welcome to Guillotine!</.dialog_description>
-        <div class="mt-4">
-          <.dialog_close_trigger class="rounded-md bg-blue-500 px-3 py-1.5 text-sm text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">
-            Close
-          </.dialog_close_trigger>
+      <div {dialog()}>
+        <button
+          class="rounded-md bg-blue-500 px-3 py-1.5 text-sm text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+          {dialog_trigger()}
+        >
+          Open dialog
+        </button>
+        <div class="absolute inset-0 w-full h-full bg-gray-200" {dialog_backdrop()}></div>
+        <div class="fixed inset-0 z-10 w-screen overflow-y-auto flex min-h-full items-center justify-center p-4" {dialog_positioner()}>
+          <div class="w-full max-w-md rounded-xl bg-white p-6 outline-0" {dialog_content()}>
+            <h2 class="text-base font-medium" {dialog_title()}>Dialog</h2>
+            <span class="mt-2 text-sm" {dialog_description()}>Welcome to Guillotine!</span>
+            <div class="mt-4">
+              <button
+                class="rounded-md bg-blue-500 px-3 py-1.5 text-sm text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
+                {dialog_close_trigger()}
+              >
+                Close
+              </button>
+            </div>
+          </div>
         </div>
-      </.dialog_content>
-    </.dialog_positioner>
-  </.dialog>
+      </div>
   ```
   """
 
   use Phoenix.Component
 
-  attr :id, :string, doc: "ID"
-  attr :as, :any, default: "div", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  @doc """
-
-  """
-  def dialog(assigns) do
-    assigns = assign_new(assigns, :id, fn -> Guillotine.nanoid() end)
-
-    Guillotine.render_as_tag_or_component(assigns, %{"phx-hook" => "Dialog"})
+  def dialog do
+    %{"phx-hook" => "Dialog"}
   end
 
-  attr :as, :any, default: "button", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  def dialog_trigger(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "trigger"})
+  def dialog_trigger do
+    %{"data-part" => "trigger"}
   end
 
-  attr :as, :any, default: "div", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-
-  def dialog_backdrop(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "backdrop"})
+  def dialog_backdrop do
+    %{"data-part" => "backdrop"}
   end
 
-  attr :as, :any, default: "div", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  def dialog_positioner(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "positioner"})
+  def dialog_positioner do
+    %{"data-part" => "positioner"}
   end
 
-  attr :as, :any, default: "div", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  def dialog_content(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "content"})
+  def dialog_content do
+    %{"data-part" => "content"}
   end
 
-  attr :as, :any, default: "h2", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  def dialog_title(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "title"})
+  def dialog_title do
+    %{"data-part" => "title"}
   end
 
-  attr :as, :any, default: "span", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  def dialog_description(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "description"})
+  def dialog_description do
+    %{"data-part" => "description"}
   end
 
-  attr :as, :any, default: "button", doc: "Tag or component to render as"
-  attr :rest, :global, doc: "Additional attributes"
-  slot :inner_block
-
-  def dialog_close_trigger(assigns) do
-    Guillotine.render_as_tag_or_component(assigns, %{"data-part" => "close-trigger"})
+  def dialog_close_trigger do
+    %{"data-part" => "close-trigger"}
   end
 end
